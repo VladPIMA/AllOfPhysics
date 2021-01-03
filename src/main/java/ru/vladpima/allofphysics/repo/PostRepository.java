ef8b1426835dbbaeb64f403f123cf37f81b2de1d
VladPIMA <vladsukhanov@icloud.com>
2021-01-04 03:55:52 +0500

    add sort article

--- src/main/java/ru/vladpima/allofphysics/repo/PostRepository.java
+++ src/main/java/ru/vladpima/allofphysics/repo/PostRepository.java
@@ -1,7 +1,11 @@
 package ru.vladpima.allofphysics.repo;
 
+import org.hibernate.resource.beans.container.spi.BeanLifecycleStrategy;
 import org.springframework.data.repository.CrudRepository;
 import ru.vladpima.allofphysics.models.Post;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findAllByOrderByIdAsc();
 }
